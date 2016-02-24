dabc83f5d807f3aec144eb78841f4b955a0e186c
Antonio Piau <antoniopiau@Antonios-MacBook-Pro.local>
2016-02-24 14:31:47 -0600

    constraints added to description view controller

--- Assignment8/DetailViewController.swift
+++ Assignment8/DetailViewController.swift
@@ -4,17 +4,20 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     var superHeroe:SuperHeroes?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.enabled = false
     }
     
     override func viewWillAppear(animated: Bool) {
         if let sh = superHeroe{
             titleLabel.text = sh.name
-            descriptionLabel.text = sh.longDescription
+            //descriptionLabel.text = sh.longDescription
+            descriptionTextView.text = sh.longDescription
         }
     }
 
