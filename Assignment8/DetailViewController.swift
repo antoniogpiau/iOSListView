f1e8d442b1d1391849bcd42d96b7350ca3783204
Antonio Piau <antoniopiau@Antonios-MacBook-Pro.local>
2016-02-24 14:34:34 -0600

    useless objects deleted

--- Assignment8/DetailViewController.swift
+++ Assignment8/DetailViewController.swift
@@ -3,20 +3,17 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     
     var superHeroe:SuperHeroes?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.enabled = false
     }
     
     override func viewWillAppear(animated: Bool) {
         if let sh = superHeroe{
             titleLabel.text = sh.name
-            //descriptionLabel.text = sh.longDescription
             descriptionTextView.text = sh.longDescription
         }
     }
